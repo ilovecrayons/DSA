8092fe492cfc2685e36fa808421679f610434e51
ilovecrayons <huangfeiyang2008@gmail.com>
2024-10-15 12:26:10 -0700

    FULL IMPL

--- Unit 1/Calculator/src/NodeTest.java
+++ Unit 1/Calculator/src/NodeTest.java
@@ -1,3 +1,43 @@
-public class NodeTest {
-    
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeTest {
+
+    @Test
+    void testRawNodeCreation() {
+        RawNode node = RawNode.createNode("Test");
+        assertNotNull(node);
+        assertEquals("Test", node.getRawContent());
+    }
+
+    @Test
+    void testRawNodeAddNext() {
+        RawNode node1 = RawNode.createNode("Node 1");
+        RawNode node2 = RawNode.createNode("Node 2");
+        node1.addNext(node2);
+        assertEquals(node2, node1.getNext());
+    }
+
+    @Test
+    void testRawNodeAddPrev() {
+        RawNode node1 = RawNode.createNode("Node 1");
+        RawNode node2 = RawNode.createNode("Node 2");
+        node1.addPrev(node2);
+        assertEquals(node2, node1.getPrev());
+    }
+
+    @Test
+    void testNumNodeCreation() {
+        NumNode node = NumNode.createNode("10.5");
+        assertNotNull(node);
+        assertEquals(10.5, node.getNumValue(), 0.01);
+    }
+
+    @Test
+    void testOpNodeCreation() {
+        OpNode node = OpNode.createNode("+");
+        assertNotNull(node);
+        assertEquals(OpNode.OpCode.ADDITION, node.getOpCode());
+        assertEquals(OpNode.OpPrio.LOWER, node.getOpPrio());
+    }
 }
